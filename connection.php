ea6cd485022155b6b6adcc88515a8f5f24f48c8b
Harsh Vardhan <54477764+harsh-vardhan152@users.noreply.github.com>
2021-04-19 18:21:11 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,1 +1,8 @@
 
+<?php  
+     	$servername = "34.203.204.238";
+	$username = "root";  
+       	$password = "root";  
+       	$conn = mysqli_connect ($servername , $username , $password) or die("unable to connect to host");  
+	$sql = mysqli_select_db ('travel',$conn) or die("unable to connect to database");	
+?>
