76d60cf7018563fc7163df1096d9e34201617f3f
Vishv Prakash Rai <59964254+vishvprakashrai@users.noreply.github.com>
2021-04-22 20:10:11 +0530

    Update localhost.sql

--- localhost.sql
+++ localhost.sql
@@ -1,1 +1,81 @@
 
+
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 02, 2020 at 04:58 AM
+-- Server version: 5.5.20
+-- PHP Version: 5.4.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `travel`
+--
+CREATE DATABASE IF NOT EXISTS `travel` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `travel`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `destination`
+--
+
+CREATE TABLE IF NOT EXISTS `destination` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `price` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `destination`
+--
+
+INSERT INTO `destination` (`id`, `name`, `price`) VALUES
+(1, 'Delhi', 300),
+(2, 'Agra', 150),
+(3, 'Mathura', 25),
+(4, 'Vrindavan', 15),
+(5, 'Barsana', 50),
+(6, 'Chaumuhan', 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pass`
+--
+
+CREATE TABLE IF NOT EXISTS `pass` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `contact` varchar(13) NOT NULL,
+  `date` date NOT NULL,
+  `dest` text NOT NULL,
+  `paid` float NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=108 ;
+
+--
+-- Dumping data for table `pass`
+--
+
+INSERT INTO `pass` (`id`, `name`, `email`, `contact`, `date`, `dest`, `paid`, `password`) VALUES
+(104, 'test1', 'h@h.c', '1234567', '2019-12-07', 'Agra', 1200, 'abcd'),
+(107, 'test2', 'jljk@gmial.com', 'jlj', '2019-12-01', 'Agra', 0, '12345');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
