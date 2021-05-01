f73c143fee195907ec2e65a47a48e9472beeb409
Vishv Prakash Rai <59964254+vishvprakashrai@users.noreply.github.com>
2021-05-01 13:50:46 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,1 +1,8 @@
 
+<?php  
+     	$servername = "localhost";
+		$username = "root";  
+       	$password = "";  
+       	$conn = mysqli_connect ($servername , $username , $password) or die("unable to connect to host");  
+	$sql = mysqli_select_db ($conn ,'travel') or die("unable to connect to database");	
+?>
